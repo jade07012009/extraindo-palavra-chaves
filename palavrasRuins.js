13de439cbeb8ee156d52f5856108053ea0251898
jade07012009 <maria.veiga.silva07@escola.pr.gov.br>
2025-09-25 11:40:36 -0300

    .

---
+++ palavrasRuins.js
@@ -0,0 +1,28 @@
+Cachorros
+
+Lealdade
+
+Amizade incondicional
+
+Animais sociais
+
+Melhores amigos do homem
+
+Companheiros fiéis
+
+Percepção das emoções
+
+Conforto
+
+Alegria
+
+Energia contagiante
+
+
+Caminhadas
+
+Brincadeiras
+
+Felicidade
+
+Bem-estar
